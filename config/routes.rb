b1fa4fbc3a6b82c2171e30736661da4556153c29
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2023-06-03 20:15:11 -0400

    More REST

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,11 @@ Rails.application.routes.draw do
              }
   get '/test', to: 'test#show'
 end
+
+namespace :api do
+  namespace :v1 do
+    resources :members do
+      resources :facts
+    end
+  end
+end
